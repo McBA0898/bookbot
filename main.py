1bcf12d89f019f6b3e7aef158c970195d61a498e
McBA0898 <bmccaleb803@gmail.com>
2025-03-04 12:51:18 -0600

    add stats.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -10,10 +12,6 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
     
-def count_words(text):
-    words = text.split()
-    return len(words)
-
 def count_characters(text):
     text_lower = text.lower()
     character_counts = {}
